c5ed0e972ed174a2811805f9209b76aebc1d9830
bear <15223245@qq.com>
2016-06-20 12:28:52 +0800

    push orign

---
+++ CCModel/Controllers/QueryTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  QueryTableViewController.h
+//  CCModel
+//
+//  Created by bear on 16/6/17.
+//  Copyright © 2016年 bear. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface QueryTableViewController : UITableViewController
+
+@property (nonatomic, assign)   int                        type;
+
+@end
---
+++ CCModel/Controllers/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  CCModel
+//
+//  Created by bear on 16/6/17.
+//  Copyright © 2016年 bear. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
